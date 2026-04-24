ea9f6f3f0962786384dea0da213554d2dc5c57de
Yan <eng.ydsouza@gmail.com>
2026-04-24 12:15:55 -0300

    commit inicial

---
+++ src/test/java/com/yandaniel/notificacao/NotificacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yandaniel.notificacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
